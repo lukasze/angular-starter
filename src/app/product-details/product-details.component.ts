ef67767b5e05b6282858b311a830f20ecd91ed20
lukasze <wyspianski.lukasz@gmail.com>
2022-11-09 13:38:58 +0100

    get the product id from the current route

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
-import { Product, products} from '../products';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
-export class ProductDetailsComponent {
-
+export class ProductDetailsComponent implements OnInit {
   priduct: Product | undefined;
 
-  constructor(private route: ActivatedRoute){}
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const productOdFromRoute = Number(routeParams.get('productId'));
+  }
 }
